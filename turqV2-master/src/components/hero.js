1c8f9b4a801d1fb0450810a8f3329fa6f03a5d4b
srikanta <srikanta.ku.subudhi@gmail.com>
2021-05-23 11:18:59 +0530

    firepad firepad_implement

--- turqV2-master/src/components/hero.js
+++ turqV2-master/src/components/hero.js
@@ -9,7 +9,7 @@ const Hero = ({header, subtext, link, buttonText}) => (
   <Grid container  alignItems="center" className="hero-bg">
   <div className="hero-blur">
     <Grid container item direction="row" justify="center" alignItems="center">
-      <Grid item xs={12} sm={6}>
+      <Grid item xs={12} sm={12} md={6} lg={6}>
         <h2 className="hero-header mt-4">{header}</h2>
         <p className="hero-subtitle mt-4">{subtext}</p>
         <p className="hero-button mt-4">
@@ -19,7 +19,7 @@ const Hero = ({header, subtext, link, buttonText}) => (
         </p>
 
       </Grid>
-      <Grid item xs={12} sm={6} justify="center" alignItems="center" className="hero-video mt-4"><iframe width="90%" height="415" src="https://www.youtube.com/embed/nA79ZnhbRMM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></Grid>
+      <Grid item xs={12} sm={12} md={6} lg={6} justify="center" alignItems="center" className="hero-video mt-4"><iframe width="96%"  src="https://www.youtube.com/embed/nA79ZnhbRMM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></Grid>
     </Grid>
     </div>
   </Grid>
